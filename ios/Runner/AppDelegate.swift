dcaef96648513b4e670893f2f93b0b43c1eb2bd9
Jack Gibbons <jack@resolutionapp.co.nz>
2026-03-09 20:54:59 +1300

    Fix build, add apns token copy

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,42 @@
 import Flutter
 import UIKit
+import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  var apnsToken: String?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in }
+    application.registerForRemoteNotifications()
+    return result
+  }
+
+  override func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    apnsToken = deviceToken.map { String(format: "%02x", $0) }.joined()
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    let messenger = engineBridge.pluginRegistry.registrar(forPlugin: "ApnsPlugin")!.messenger()
+    let channel = FlutterMethodChannel(name: "com.example.apns/token", binaryMessenger: messenger)
+    channel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+      guard call.method == "getApnsToken" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
+      if let token = self?.apnsToken {
+        result(token)
+      } else {
+        result(FlutterError(code: "UNAVAILABLE", message: "APNS token not yet available", details: nil))
+      }
+    }
   }
 }
